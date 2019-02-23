68abd5531a6421b34fff5190b30b024d5da03fe6
Martin Hencl <martin.hencl@email.cz>
2019-02-23 20:07:54 +0100

    Čtvrý příklad

--- Cv1Solution/cv1_podleZadani/Program.cs
+++ Cv1Solution/cv1_podleZadani/Program.cs
@@ -8,7 +8,7 @@ namespace cv1_podleZadani
 {
     class Program
     {
-        static void funkceRodneCislo()
+        static void FunkceRodneCislo()
         {
             Console.WriteLine("Příklad 3 RČ");
             StringBuilder rcBuilder = new StringBuilder("911213/1234");
@@ -25,6 +25,38 @@ namespace cv1_podleZadani
             }
             Console.WriteLine("");
             Console.WriteLine("*********************");
+        }
+
+        private static bool HadaniCisla(int pocetHadani)
+        {
+            Random nahoda = new Random();
+            int nahodneCislo = nahoda.Next(101);
+            int hadaneCislo = -1;
+            int iteraceHadani = 0;
+            while (true)
+            {
+                iteraceHadani++;
+                Console.Write("Pokus č. {0} z {1}:", iteraceHadani, pocetHadani);
+                int.TryParse(Console.ReadLine(), out hadaneCislo);
+                if (hadaneCislo < nahodneCislo)
+                {
+                    Console.WriteLine("Hadane cislo je vetsi nez tipnute {0}", hadaneCislo);
+                }
+                else if (hadaneCislo > nahodneCislo)
+                {
+                    Console.WriteLine("Hadane cislo je mensi nez tipnute {0}", hadaneCislo);
+                }
+                else if (hadaneCislo == nahodneCislo)
+                {
+                    Console.WriteLine("Bingo! Cislo je {0}", nahodneCislo);
+                    return true;
+                }
+                if (iteraceHadani > pocetHadani)
+                {
+                    Console.WriteLine("Prekroceny pocet pokusu, prohra!");
+                    return false;
+                }
+            }
         }
 
         static void Main(string[] args)
@@ -50,9 +82,19 @@ namespace cv1_podleZadani
             Console.WriteLine("*********************");
 
             /* pr 3 Rč */
-            funkceRodneCislo();
+            FunkceRodneCislo();
 
-            /* pr 4 */
+            /* pr 4 hadani cisla    */
+            while (true)
+            {
+                HadaniCisla(10);
+                Console.Write("Opakovat? [ano/ne] ('ne' pri prazdnem vstupu)");
+                string nactene = Console.ReadLine();
+                if (nactene.Equals("ne") || nactene.Equals(""))
+                {
+                    break;
+                }
+            }
         }
     }
 }
