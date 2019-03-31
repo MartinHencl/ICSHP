895ab0f86bca3318e31776d9b65b21c0fb66406d
Martin Hencl <martin.hencl@email.cz>
2019-03-31 22:27:04 +0200

    Kostra

---
+++ Excersise05/cv2018_06-LigaMistru-kostra/LigaMistru/Entity/SpojovySeznam.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LigaMistru
+{
+    // Spojový seznam by měl realizovat rozhraní IEnumerable, ICollection a IList
+    // Shoddu prvků testujte pomocí metody Equals
+    // Doplňte Equals do třídy Hrac
+
+    public class SpojovySeznam
+    {
+        // ...
+    }
+}
